e94e196143b9d28a88fb42fe9fb76756438e6e09
Ankur Mathur <ankurakaet@gmail.com>
2021-07-19 21:48:53 +0530

    Initial Commit

---
+++ blog/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'Ankur Mathur - A Full Stack Developer (.NET)';
+}
