d1b59bc12032cbdace666b9106ec67fa6a1303e7
Ankur Mathur <ankurakaet@gmail.com>
2021-08-03 23:47:34 +0530

    changes

--- blog/src/app/home/home.component.ts
+++ blog/src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+type MonthYearType = {
+  month: number;
+  year: number;
+};
+
 type CVData = {
   name: String;
   profile: String;
@@ -11,12 +16,16 @@ type CVData = {
   website?: string;
   careerSummary: string;
   workExperience: {
-    position: string;
     company: string;
-    year1: string;
-    year2: string;
-    roleDescription: string;
+    logo: string;
     technologies?: string[];
+    timeline?: {
+      position: string;
+      from: MonthYearType;
+      to?: MonthYearType;
+      roleDescription: string[];
+      location?: string;
+    }[];
   }[];
   skills?: {
     name: string;
@@ -53,6 +62,82 @@ export class HomeComponent implements OnInit {
       workExperience: [
         {
           company: 'Nagarro',
+          logo: 'assets/nagarro_logo_new.svg',
+          technologies: [
+            'Angular',
+            '.NET Core',
+            '.Net Framework',
+            'ASP.NET MVC',
+            'SQL Server',
+            'Azure Cloud',
+          ],
+          timeline: [
+            {
+              position: 'Associate Technical Lead',
+              roleDescription: [
+                'NodeJS and Angular Applications',
+                'Explored the glorious field of Data Science and Machine Learning !',
+                'Heavy use of SPA architecture, Azure cloud infrastructure and Angular/Node.',
+                'dotNET Core for function apps and critical cloud infrastructure.',
+                'SQL Server 2016 and Azure SQL as backend.`',
+              ],
+              from: {
+                month: 11,
+                year: 2018,
+              },
+              location: 'Jaipur, Rajasthan, India',
+            },
+            {
+              position: 'Senior Associate Technology',
+              roleDescription: [
+                'Worked in Education domain. Primarily for simulation and smart applications for students aged 8-12 years.',
+                'Heavy use of SPA architecture, Azure cloud infrastructure and Angular/Node.',
+                'dotNET Core for function apps and critical cloud infrastructure.',
+                'SQL Server 2016 and Azure SQL as backend.',
+              ],
+              from: {
+                month: 9,
+                year: 2017,
+              },
+              to: { month: 10, year: 2018 },
+              location: 'Jaipur, Rajasthan, India',
+            },
+            {
+              position: 'Technical Associate',
+              roleDescription: [
+                'Worked in Manufacturing domain. Six sigma quality protocol and SPC (Statistical Process Control) software development.',
+                'SPA ( Single Page Applications ) and Azure cloud infrastructure.',
+                'Technologies Used: ASP.Net 4.5, Angular 2, NodeJS, Javascript, Backbone.JS, D3JS, HTML5, CSS3, WebAPI 2, SQL Server 2014',
+              ],
+              from: {
+                month: 4,
+                year: 2016,
+              },
+              to: {
+                month: 9,
+                year: 2017,
+              },
+              location: 'Jaipur, Rajasthan, India',
+            },
+          ],
+        },
+        {
+          company: 'A3Logics',
+          logo: 'assets/a3logics_logo.png',
+          technologies: ['.Net Framework', 'ASP.NET MVC', 'SQL Server'],
+          timeline: [
+            {
+              position: 'Software Engineer',
+              roleDescription: [
+                'Worked in Health Insurance domain. US Taxation and US Health insurance policies.',
+                'X12 EDI file development (EDI 834, 837).',
+                'HR Management System Software',
+              ],
+              location: 'Jaipur, Rajasthan, India',
+              from: { month: 8, year: 2013 },
+              to: { month: 4, year: 2016 },
+            },
+          ],
         },
       ],
       skills: [],
